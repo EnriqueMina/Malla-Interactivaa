434c99d7a12e730edf2065ad1e8dcf1139dafbfe
EnriqueMina <carlosenriquesolismi9@gmail.com>
2025-07-14 23:17:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,30 +15,5 @@ document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
     } else {
       label.classList.remove('tachado');
     }
-    controlarDesbloqueo();
   });
 });
-
-function controlarDesbloqueo() {
-  const semestres = document.querySelectorAll('.semestre');
-
-  semestres.forEach((semestre, index) => {
-    const checkboxes = semestre.querySelectorAll('input[type="checkbox"]');
-    const marcadas = Array.from(checkboxes).filter(cb => cb.checked).length;
-    const total = checkboxes.length;
-
-    // Desbloquear siguiente si hay al menos 80% completado
-    const porcentaje = marcadas / total;
-    const siguiente = semestres[index + 1];
-
-    if (siguiente) {
-      const sigCheckboxes = siguiente.querySelectorAll('input[type="checkbox"]');
-      sigCheckboxes.forEach(cb => {
-        cb.disabled = porcentaje < 0.8;
-      });
-    }
-  });
-}
-
-// Ejecutar al iniciar
-controlarDesbloqueo();
