2d9e56ef7c26d814d0fd4b3d3943b7a64c95eb9b
EnriqueMina <carlosenriquesolismi9@gmail.com>
2025-07-14 23:44:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+document.querySelectorAll('input[type="checkbox"]').forEach((checkbox, index) => {
+  const id = `checkbox-${index}`;
+
+  // Cargar estado guardado
+  const saved = localStorage.getItem(id);
+  if (saved === 'true') {
+    checkbox.checked = true;
+    checkbox.parentElement.classList.add('tachado');
+  }
+
+  // Escuchar cambios y guardar
   checkbox.addEventListener('change', function () {
-    if (this.checked) {
+    localStorage.setItem(id, checkbox.checked);
+    if (checkbox.checked) {
       this.parentElement.classList.add('tachado');
     } else {
       this.parentElement.classList.remove('tachado');
